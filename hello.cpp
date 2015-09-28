f52a8530c5c560b6fa14efe4fd82de4fbde31c84
Kevin Wei <gra230434@gmail.com>
2015-09-28 13:11:03 +0800

    main

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
